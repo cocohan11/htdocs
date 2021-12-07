abd92178919075e926dbc28ae3b14f5ff83754fe
cocohan11 <rlagksdl96@naver.com>
2021-12-07 21:21:51 +0900

    php랑 ㅏmysql 시작

---
+++ php/db.php
@@ -0,0 +1,12 @@
+<?php
+$db = mysqli_connect('127.0.0.1','root','rich','work');
+
+if($db) {
+    echo 'DB접속 성공1';
+} else {
+    echo '실122222패';
+}
+
+
+
+?>
---
+++ php/resister_server.php
@@ -0,0 +1,16 @@
+<?php
+//isset : 있어야 실행함
+if (isset($_POST['id'])&&isset($_POST['nick'])&&isset($_POST['pass1'])&&isset($_POST['pass2'])) {
+// 실행시켜    
+// 보안을 더욱 강화
+    // mysql_real
+// 에러를 체크
+// 저장시키는 코딩
+
+} else {
+    // 에러메시지
+    // gg
+}
+
+
+?>
