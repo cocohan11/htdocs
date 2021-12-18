4af9baecb61a9e7fa0bc028cc714b5fa8fc5b602
cocohan11 <rlagksdl96@naver.com>
2021-12-18 21:37:59 +0900

    이미지 서버에 저장 성공

--- php/image.php
+++ php/image.php
@@ -1,44 +1,12 @@
 <!DOCTYPE html>
-
 <html>
-
-<head>
-
-    <title>Image Upload in PHP</title>
-
-    <! link the css file to style the form >
-
-    <link rel="stylesheet" type="text/css" href="style.css" />
-
-</head>
-
+   <head>
+   </head>
 <body>
-
-    <div id="wrapper">
-
-        <!-- specify the encoding type of the form using the 
-
-                enctype attribute -->
-
-        <form method="POST" action="/php/image_result.php" enctype="multipart/form-data">        
-
-            <input type="file" name="choosefile" value="" />
-
-            <div>
-
-                <button type="submit" name="uploadfile">
-
-                UPLOAD
-
-                </button>
-
-            </div>
-
-        </form>
-
-    </div>
-
+   <form action="image_result.php" method="POST" enctype="multipart/form-data">
+      <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+      <input type="file" name="file">
+      <input type="submit">
+   </form>
 </body>
-
-</html>
-
+<html>
--- php/image_result.php
+++ php/image_result.php
@@ -1,73 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+  <head>
+     <body>
+        <?php
+          echo $_FILES["file"]['name']."<br>";
+          echo $_FILES["file"]['tmp_name']."<br>";
+          echo $_FILES["file"]['size']."<br>";
+          echo $_FILES['file']['error']."<br>";
+        //   $path = '/usr/local/apache2.4/htdocs/Image/';
+          $path = '../Image/';
+        //   require "$path";
 
-error_reporting(0);
-echo '1입니다';
-?>
 
-<?php
-
-$msg = ""; 
-echo '2입니다';
-
-// check if the user has clicked the button "UPLOAD" 
-
-if (isset($_POST['uploadfile'])) {
-    echo '3입니다';
-
-    $filename = $_FILES["choosefile"]["name"];
-    echo $filename.'입니다~~';
-
-    $tempname = $_FILES["choosefile"]["tmp_name"];  
-    echo $tempname.'임시저장~~';
-
-        // $folder = "image/".$filename;   
-        $path = "/usr/local/apache2.4/htdocs/Image/";       //htdocs안에만 있어야 한다. 
-        // $location = $path . $_FILES['choosefile']['name'];
-        echo $path.'폴더~~';
-        echo "<pre>"; 
-        print_r($_FILES);
-
-    // connect with the database
-    $db = mysqli_connect("127.0.0.1", "root", "rich", "work"); 
-
-        // query to insert the submitted data
-        echo '4입니다';
-
-        // $sql = "INSERT INTO product (image) VALUES ('$filename')";
-        $sql = "insert into image (image_name) values ('$filename')";
-        echo $sql.'sql입니다';
-
-        // function to execute above query
-        $쿼리보내기 = mysqli_query($db, $sql);       
-        if($쿼리보내기){
-            echo '쿼리보내기 성공';
+        if (move_uploaded_file($_FILES['file']['tmp_name'], $path.$_FILES["file"]['name'])) {
+            echo "Uploaded";
         } else {
-            echo '쿼리보내기 실패';
+           echo "File not uploaded";
         }
 
-        // Add the image to the "image" folder"
-        // if (move_uploaded_file($tempname, $location)) {     // 임시>>지정위치 이동
+        //   move_uploaded_file($_FILES['file']['name'], $move);
+        ?>
+     <body>
+<html>
 
-        //     $msg = "Image uploaded successfully";
-        //     echo $msg;
-
-        // } else{
-
-        if (move_uploaded_file($tempname, __DIR__.$path.$filename_)) {
-            echo "Image uploaded successfully"; 
-        } else {
-            
-        $msg = "Failed to upload image";
-        echo $msg;
-        }
-
-}
-
-$result = mysqli_query($db, $sql);
-// if($result) {
-//     echo '성공ㅎ';
-// } else {
-//     echo '실패ㅡㅡ';
-// }
-
-?>
